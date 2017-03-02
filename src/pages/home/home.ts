287512d50969a0232215588a81a050595f44ed95
julbarrientos <julbarrientos@gmail.com>
2017-03-01 23:01:49 -0300

    firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,40 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import { Batalla } from '../batalla/batalla';
+
 @Component({
   selector: 'home',
   templateUrl: 'home.html'
 })
 export class Home {
 
-  constructor(public navCtrl: NavController) {
+  items: Array<{
+                P1:string,
+                P2:string,
+                jugador1:number[],
+                jugador2:number[],
+                Seljugador1:number[],
+                Seljugador2:number[],
+                acertoJ1:boolean,
+                acertoJ2:boolean,
+                monto: number,
+                turno: string
+              }>;
+  batallas:  FirebaseListObservable<any>;
+  
+  constructor(public navCtrl: NavController, af: AngularFire) {
+    this.batallas = af.database.list('/Batallas');
     
+  }
+
+  goToBatalla(event, item) {
+
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(Batalla, {
+      batalla: item
+    });
   }
 
 }
